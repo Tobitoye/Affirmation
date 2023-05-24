43026b4fadbc5f644d4b7c70dbf3d69ea83bb0d8
tobi <n1029165@my.ntu.ac.uk>
2023-05-24 16:10:37 +0100

    Simple Affirmation app

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Affirmations } from "../UniLove/affirmations";
 
 export default function App() {
+  const [currentAffirmationIndex, setCurrentAffirmationIndex] = useState(0);
+
+  useEffect(() => {
+    
+    const today = new Date().getDate();
+    setCurrentAffirmationIndex(today % Affirmations.length);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "#D8BFD8", alignItems: "center", justifyContent: "center", paddingHorizontal: 20 }}>
+      <Text style={{ fontSize: 40, color: "#524656", marginBottom: 20, fontWeight: "bold", textAlign: "center" }}>
+        Daily Affirmation  
+      </Text>
+
+      <View style={{ marginVertical: 20 }}>
+        <Text style={{ fontSize: 30, color: "#524656", textAlign: "center", lineHeight: 28 }}>
+          {Affirmations[currentAffirmationIndex].DayAffirm }&#x2728;
+        </Text>
+      </View>
+
+      
     </View>
   );
 }
@@ -16,5 +34,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#f4511e',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
---
+++ affirmations.js
@@ -0,0 +1,122 @@
+export const Affirmations = [
+    {
+        id: 1,
+        DayAffirm: 'I am beautiful in all ways'
+    },
+    {
+        id: 2,
+        DayAffirm: 'I respect who I am'
+    },
+    {
+        id: 3,
+        DayAffirm: 'I am confident in all aspects of myself'
+    },
+    {
+        id: 4,
+        DayAffirm: 'My beauty is natural'
+    },
+    {
+        id: 5,
+        DayAffirm: 'I am confident in who I am'
+    },
+    {
+        id: 6,
+        DayAffirm: 'I release all negativity'
+    },
+    {
+        id: 7,
+        DayAffirm: 'I fill my mind with positive thoughts'
+    },
+    {
+        id: 8,
+        DayAffirm: 'I attract so much beauty into my life'
+    },
+    {
+        id: 9,
+        DayAffirm: 'I am healthy and happy'
+    },
+    {
+        id: 10,
+        DayAffirm: 'I love myself just the way I am'
+    },
+    {
+        id: 11,
+        DayAffirm: 'I am comfortable in my skin'
+    },
+    {
+        id: 12,
+        DayAffirm: 'I like the person that I’m becoming'
+    },
+    {
+        id: 13,
+        DayAffirm: 'I love all aspects of my body'
+    },
+    {
+        id: 14,
+        DayAffirm: 'I see the beauty within and without'
+    },
+    {
+        id: 15,
+        DayAffirm: 'I think positive thoughts about myself and others'
+    },
+    {
+        id: 16,
+        DayAffirm: 'I validate myself daily'
+    },
+    {
+        id: 17,
+        DayAffirm: 'I have an attractive mind, body, and spirit'
+    },
+    {
+        id: 18,
+        DayAffirm: 'I like the person that I see in the mirror'
+    },
+    {
+        id: 19,
+        DayAffirm: 'Feeling good about myself is my natural birthright'
+    },
+    {
+        id: 20,
+        DayAffirm: 'I am powerful enough to overcome all negativity in my life'
+    },
+    {
+        id: 21,
+        DayAffirm: 'I confront what I need to do with love in my heart'
+    },
+    {
+        id: 22,
+        DayAffirm: 'I deal with what life throws at me'
+    },
+    {
+        id: 23,
+        DayAffirm: 'I have the inner strength to face life head-on'
+    },
+    {
+        id: 24,
+        DayAffirm: 'I have all the willpower I need'
+    },
+    {
+        id: 25,
+        DayAffirm: 'Each day I focus on my inner strength'
+    },
+    {
+        id: 26,
+        DayAffirm: 'Life presents to me joyous circumstances'
+    },
+    {
+        id: 27,
+        DayAffirm: 'I love myself, and I am worthy to have goodness in my life'
+    },
+    {
+        id: 28,
+        DayAffirm: 'I am involved only in healthy relationships'
+    },
+    {
+        id: 29,
+        DayAffirm: 'I am in harmony with nature'
+    },
+    {
+        id: 30,
+        DayAffirm: 'I choose to be at peace'
+    },
+]
